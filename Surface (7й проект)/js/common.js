1ccc1a197b00c4e8370786a1b3e9ad0b6b50f161
Farrukh <fa.khusanov@gmail.com>
2021-04-22 14:05:07 +0500

    Add files via upload

---
+++ Surface (7й проект)/js/common.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        768:{
+            items:3,
+            nav:false
+        }
+    }
+})
